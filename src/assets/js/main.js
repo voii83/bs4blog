d12c186f2e397fa50c25f3cf2af0cfdbff349e07
voii83 <voiivoii83@gmail.com>
2018-11-11 21:30:16 +0300

    first commit

---
+++ src/assets/js/main.js
@@ -0,0 +1,9 @@
+//= ../../../node_modules/jquery/dist/jquery.js
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/tooltip.js
+//= ../../../node_modules/bootstrap/js/dist/popover.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+
+//= custom/tooltips.js
+//= custom/popover.js
